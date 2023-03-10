2bc3214e5739681a8f04428f0b62db31d453c32f
Clément Roque <clement@tarmactechnologies.com>
2023-03-10 10:13:05 +0100

    Add js algorithm to refactor

---
+++ bubbleSort.js
@@ -0,0 +1,14 @@
+export const bubbleSort = (t) => { 
+    const n = t.length;
+    for (let i=0; i<n; i++) {
+        for (let j = 0; j < (n - i - 1); j++) {
+            if (t[j] > t[j + 1]) {
+                let a = t[j + 1];
+                let b = t[j];
+                t[j] = a;
+                t[j + 1] = b;
+            };
+        };
+    };
+    return t
+};
